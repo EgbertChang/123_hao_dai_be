2be6f4d3ab00f2cd3873a28685141fb67fae6fd4
egbertchang <egbertchang@icloud.com>
2018-07-09 14:10:31 +0800

    style: fmt

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-  "123_hao_dai/elea"
-  "123_hao_dai/src/server"
-  "runtime"
+	"123_hao_dai/elea"
+	"123_hao_dai/src/server"
+	"runtime"
 )
 
 func init() {
-  runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
-  Server := elea.Server{
-    Addr: ":8080",
-    Handle: server.Handle(),
-    Interceptor: &server.HttpInterceptor{},
-  }
-  Server.ListenAndServer()
+	Server := elea.Server{
+		Addr:        ":8080",
+		Handle:      server.Handle(),
+		Interceptor: &server.HttpInterceptor{},
+	}
+	Server.ListenAndServer()
 }
